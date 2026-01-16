856d6238ba8a5f7aee92b87f35084686bd5e0c46
Muse-g <Mussiehailug@gmail.com>
2026-01-16 12:04:04 +0100

    Create S1_VAR_01.js

---
+++ js/task1/S1_VAR_01.js
@@ -0,0 +1,10 @@
+// S1_VAR_01 - Variable Table
+var course = "Cloud Programming";
+let year = 2026;
+const isFun = true;
+
+console.table([
+  { Name: "course", Value: course, Type: typeof course },
+  { Name: "year", Value: year, Type: typeof year },
+  { Name: "isFun", Value: isFun, Type: typeof isFun }
+]);
